3887c6e8d4c5e2f21dfcf97920b96eadb0a87c41
citradfd <contact.citradfd@gmail.com>
2024-07-18 16:18:39 +0700

    ganti input jadi select

--- resources/views/editData.blade.php
+++ resources/views/editData.blade.php
@@ -5,7 +5,11 @@
     <input type="hidden" value="{{$ID}}">
     Nama: <input type="text" name="Nama" value="{{$Pegawai->Nama}}"><br>
     NIK: <input type="text" name="NIK" value="{{$Pegawai->NIK}}"><br>
-    ID Departemen: <input type="number" name="id_departemen" value="{{$Pegawai->id_departemen}}" min="1" max="3"><br>
+Departemen: <select name="id_departemen">
+            @foreach ($departemen as $data)
+            <option value="{{$data->id}}" @if ($data->id == $Pegawai->id_departemen) selected @endif>{{$data->nama_departemen}}</option>
+            @endforeach
+        </select><br>
     Tanggal Lahir: <input type="date" name="tgl_lahir" value="{{$Pegawai->tgl_lahir}}"><br>
     Alamat: <input type="text" name="alamat" value="{{$Pegawai->alamat}}"><br>
     <button type="submit">Update</button>
